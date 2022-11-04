95399cbf9620897177b5d0ba4e2eff40c830bd2a
DzakirinMD <dzakirinmd@gmail.com>
2022-11-04 11:10:37 +0800

    Added Unit Test

---
+++ src/test/kotlin/net/springbootkotlin/IntegrationTests.kt
@@ -0,0 +1,41 @@
+package net.springbootkotlin
+
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.HttpStatus
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
+
+    @BeforeAll
+    fun setup() {
+        println(">> Setup")
+    }
+
+    @Test
+    fun `Assert blog page title, content and status code`() {
+
+        // Get what url to be tested
+        val entity = restTemplate.getForEntity<String>("/")
+
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(entity.body).contains("<h1>Blog Application</h1>")
+    }
+
+    @Test
+    fun `Assert article page title, content and status code`() {
+        println(">> TODO")
+    }
+
+    @AfterAll
+    fun teardown() {
+        println(">> Tear Down")
+    }
+
+}
