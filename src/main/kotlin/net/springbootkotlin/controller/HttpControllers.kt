05e09cd3a5a871d65c31aac10ee75325e0be731e
DzakirinMD <dzakirinmd@gmail.com>
2022-11-09 08:58:22 +0800

    expose HTTP API

---
+++ src/main/kotlin/net/springbootkotlin/controller/HttpControllers.kt
@@ -0,0 +1,33 @@
+package net.springbootkotlin.controller
+
+import net.springbootkotlin.repositories.ArticleRepository
+import net.springbootkotlin.repositories.UserRepository
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
+
+@RestController
+@RequestMapping("/api/articles")
+class ArticlesController (private val repository: ArticleRepository) {
+
+    @GetMapping("/")
+    fun findAll() = repository.findAllByOrderByAddedAtDesc()
+
+    @GetMapping("/{slug}")
+    fun findOne(@PathVariable slug: String) = repository.findBySlug(slug) ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "This article does not exist")
+}
+
+@RestController
+@RequestMapping("/api/users")
+class Users (private val repository: UserRepository) {
+
+    @GetMapping("/")
+    fun findAll() = repository.findAll()
+
+    @GetMapping("/{login}")
+    fun findOne(@PathVariable login: String) = repository.findByLogin(login) ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "This user does not exist")
+}
+
